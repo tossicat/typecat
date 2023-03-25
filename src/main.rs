f3c02fc2140f4a2e7f70b70538a447768894ca80
Jaehwan <dialektike@gmail.com>
2023-03-25 17:43:35 +0900

    라이브러리에 `TOML` 파서 추가 및 테스트 코드 작성

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use serde::Deserialize;
+
 fn main() {
-    println!("Hello, world!");
+    #[derive(Deserialize)]
+    struct Config {
+        ip: String,
+        port: Option<u16>,
+        keys: Keys,
+    }
+
+    #[derive(Deserialize)]
+    struct Keys {
+        github: String,
+        travis: Option<String>,
+    }
+
+    let config: Config = toml::from_str(
+        r#"
+   ip = '127.0.0.1'
+
+   [keys]
+   github = 'xxxxxxxxxxxxxxxxx'
+   travis = 'yyyyyyyyyyyyyyyyy'
+"#,
+    )
+    .unwrap();
+
+    println!("{:?}", config.ip);
+    println!("{:?}", config.port);
+    println!("{:?}", config.keys.github);
+    println!("{:?}", config.keys.travis.as_ref().unwrap());
 }
