2bde38ca76b62ebef8e0851609321db67c91d811
Jaehwan <dialektike@gmail.com>
2023-04-08 15:06:44 +0900

    toml 형식 파일 읽기 테스트

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
+use std::fs::File;
+use std::io::{Error, Read, BufReader};
+
 use serde::Deserialize;
 
-fn main() {
-    #[derive(Deserialize)]
-    struct Config {
-        ip: String,
-        port: Option<u16>,
-        keys: Keys,
-    }
+#[derive(Deserialize)]
+struct Config {
+    ip: String,
+    port: Option<u16>,
+    keys: Keys,
+}
 
-    #[derive(Deserialize)]
-    struct Keys {
-        github: String,
-        travis: Option<String>,
-    }
+#[derive(Deserialize)]
+struct Keys {
+    github: String,
+    travis: Option<String>,
+}
 
-    let config: Config = toml::from_str(
-        r#"
-   ip = '127.0.0.1'
-
-   [keys]
-   github = 'xxxxxxxxxxxxxxxxx'
-   travis = 'yyyyyyyyyyyyyyyyy'
-"#,
-    )
-    .unwrap();
-
+fn main() -> Result<(), Error> {
+    let file_name = "themes/test.toml";
+    let file = File::open(file_name)?;
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents)?;
+    // print!("{}", contents);
+    let config: Config = toml::from_str(&contents).unwrap();
     println!("{:?}", config.ip);
     println!("{:?}", config.port);
     println!("{:?}", config.keys.github);
     println!("{:?}", config.keys.travis.as_ref().unwrap());
+    Ok(())
 }
