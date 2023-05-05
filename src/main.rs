6779162f15124ae9674386f16963ec9f15ad58a1
Jaehwan <dialektike@gmail.com>
2023-05-05 15:07:13 +0900

    CMD를 만들기 위해 `clap` 적용 & 테스트

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,17 @@ use pest::Parser;
 #[grammar = "markdown.pest"]
 pub struct MarkdownParser;
 
+// CMD로 작동하기 위한 코드 시작
+use clap::Parser as clap_parser;
+
+#[derive(clap_parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    file_names: Vec<String>,
+}
+// CMD로 작동하기 위한 코드 끝
+
 fn main() {
-    // toml 형식 파일 테스트 시작
-    let toml_file_name = "test.toml";
-    let test = read_theme_file(&toml_file_name.to_owned());
-    match test {
-        Ok(s) => println!("{:?}", parsing_toml(s)),
-        Err(e) => println!("{:?}", e),
-    };
-    // toml 형식 파일 테스트 끝
     let unparsed_file = read_to_string("test/test.md").expect("cannot read file");
     let file = MarkdownParser::parse(Rule::FILE, &unparsed_file)
         .expect("unsuccessful parse")
@@ -32,4 +34,15 @@ fn main() {
             println!("{:?}", sentence);
         }
     }
+    let cli = Cli::parse();
+    println!("files_name: {:?}", cli.file_names);
+
+    // toml 형식 파일 테스트 시작
+    let toml_file_name = "test.toml";
+    let test = read_theme_file(&toml_file_name.to_owned());
+    match test {
+        Ok(s) => println!("{:?}", parsing_toml(s)),
+        Err(e) => println!("{:?}", e),
+    };
+    // toml 형식 파일 테스트 끝
 }
