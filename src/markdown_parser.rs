d6744b3a5df3d12122e7e63637d72bed32637180
dev-bearabbit <cjstk3940@naver.com>
2023-10-21 17:29:45 +0900

    add test md parser

--- src/markdown_parser.rs
+++ src/markdown_parser.rs
@@ -8,26 +8,65 @@ use pest::iterators::Pair;
 pub struct MarkdownParser;
 
 pub fn parse_markdown() {
-    let unparsed_file = read_to_string("test/test.md").expect("cannot read file");
+    let unparsed_file = read_to_string("test/pdf.md").expect("cannot read file");
     let file = MarkdownParser::parse(Rule::FILE, &unparsed_file)
         .expect("unsuccessful parse")
         .next()
         .unwrap();
     for line in file.into_inner() {
-            if line.as_rule() == Rule::HEADER {
+        match line.as_rule() {
+            Rule::HEADER => {
+                println!("################################");
                 parse_header(line);
-                println!("###############################")
-            }
-            else {
-                parse_sentence(line);
-            }
+            },
+            Rule::TABLE_ALIGN | Rule::TABLE_LINE => {
+                println!("###############################");
+                parse_table(line);
+            },
+            Rule::LIST |  Rule::ORDERD_LIST => {
+                println!("###############################");
+                parse_list(line);
+            },
+            Rule::CODE_BLOCK => {
+                println!("###############################");
+                parse_code(line);
+            },
+            Rule::LINE => {
+                println!("###############################");
+                parse_line(line);
+            },
+            _ => {}
         }
+    }
 }
 
 fn parse_header(header: Pair<Rule>) {
-    println!("{:?}", header);
+    for line in header.into_inner(){
+        if line.as_rule() == Rule::LINE {
+            parse_line(line);
+        }
+        else {
+            println!("{:?}", line.as_rule());
+        }
+    }
 }
 
-fn parse_sentence(sentence: Pair<Rule>) {
-    println!("{:?}", sentence);
+fn parse_line(content: Pair<Rule>) {
+    //contents, styled, link
+    println!("{:?}", content);
+}
+
+fn parse_table(content: Pair<Rule>) {
+    //TABLE_WORD, LEFT_ALIGNED, CENTER_ALIGNED, RIGHT_ALIGNED
+    println!("{:?}", content);
+}
+
+fn parse_code(content: Pair<Rule>) {
+    //contents, styled, link
+    println!("{:?}", content);
+}
+
+fn parse_list(content: Pair<Rule>) {
+    //contents, styled, link
+    println!("{:?}", content);
 }
--- tests/toml_file.rs
+++
@@ -1,7 +0,0 @@
-// use typecat::manage_toml_file;
-
-// #[test]
-// fn test_toml_file_and_path() {
-//     let temp_path = "themes/test.toml";
-//     assert_eq!(true, manage_toml_file(temp_path.to_owned()));
-// }
