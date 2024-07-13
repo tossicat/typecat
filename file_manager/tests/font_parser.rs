1f69c785663eec6d4419dbf1b8b30eb3684f899d
Jaehwan <dialektike@gmail.com>
2024-07-13 17:53:29 +0900

    "../assets/fonts"에 들어 있는 폰트 읽어 오기

--- file_manager/tests/font_parser.rs
+++ file_manager/tests/font_parser.rs
@@ -1,5 +1,4 @@
 const DEMO_TTF: &[u8] = include_bytes!("./fonts/NanumGothic.ttf");
-const FOT_DIR:&str = "./fonts";
 
 use fontdb;
 use std::sync::Arc;
@@ -15,4 +14,20 @@ fn add_font_and_get_ids_back() {
     let font = font_db.face(id).unwrap();
     println!("reading `{:?}`", ids);
     assert!(font.families.iter().any(|(name, _)| name == "NanumGothic"));
+    println!("reading `{:?}`", font.style);
+}
+
+#[test]
+fn add_fonts_db() {
+    pub const DEFAULT_FONT_FOLDER: &str = "../assets/fonts";
+    let mut font_db = fontdb::Database::new();
+    font_db.load_fonts_dir(DEFAULT_FONT_FOLDER);
+    let now = std::time::Instant::now();
+
+    println!(
+        "Loaded {} font faces in {}ms.",
+        font_db.len(),
+        now.elapsed().as_millis()
+    );
+    println!("reading {:?}", font_db);
 }
