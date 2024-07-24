5b706f9abb12a13034985ba8602eb50e311b5e39
Jaehwan <dialektike@gmail.com>
2024-07-24 13:42:04 +0900

    `loading_font_lists_into_db_in_assets_folder()` 함수 작성 시작

--- file_manager/src/font_parser.rs
+++ file_manager/src/font_parser.rs
@@ -1,17 +1,39 @@
-const DEMO_TTF: &[u8] = include_bytes!(" ../assets/fonts/NanumGothic.ttf");
-use std::sync::Arc;
 use fontdb;
 
-#[test]
-fn add_fonts_and_get_ids_back() {
-    env_logger::init();
+const _DEFAULT_FONT_FOLDER: &str = "../assets/fonts";
+
+#[derive(Debug)]
+pub struct Font {
+    name: String,
+    is_bold_exist: bool,
+    is_italic_exist: bool,
+}
+
+// fn is_italic_exist() = {
+//     match item.style {
+//         fontdb::Style::Normal => todo!(),
+//         fontdb::Style::Italic => todo!(),
+//         fontdb::Style::Oblique => todo!(),
+//     }
+// };
+
+pub fn loading_font_lists_into_db_in_assets_folder() -> Vec<Font> {
     let mut font_db = fontdb::Database::new();
-    let ids = font_db.load_font_source(fontdb::Source::Binary(Arc::new(DEMO_TTF)));
+    font_db.load_fonts_dir(_DEFAULT_FONT_FOLDER);
 
-    assert_eq!(ids.len(), 1);
-    let id = ids[0];
+    let mut font_list: Vec<Font> = vec![];
 
-    let font = font_db.face(id).unwrap();
-    println!("reading `{}`", font);
-    assert!(font.families.iter().any(|(name, _)| name == "NanumGothic"));
+    for item in font_db.faces() {
+        // println!("{:?}", item.families);
+        for sub_item in &item.families {
+            // println!("{:?}", sub_item);
+            let temp_font = Font {
+                name: sub_item.0.clone(),
+                is_bold_exist: true,
+                is_italic_exist: true,
+            };
+            font_list.push(temp_font);
+        }
+    }
+    font_list
 }
